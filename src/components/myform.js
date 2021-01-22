273e35098037b96aad08ff47080f621c5131fdd5
unknown <Bryan@SJ-BHERRERA-XPS.netgear.com>
2021-01-22 13:54:11 -0800

    center contact button

--- src/components/myform.js
+++ src/components/myform.js
@@ -14,35 +14,34 @@ export default class MyForm extends React.Component {
   render() {
     const { status } = this.state
     return (
-      <ContentWrapper>
-        <form
-          onSubmit={this.submitForm}
-          action="https://formspree.io/f/mleoznyk"
-          method="POST"
-        >
-          <div className="form-group">
-            <label>Name:</label>
-            <input type="text" className="form-control" name="name" />
-          </div>
-          <div className="form-group">
-            <label>Email:</label>
-            <input type="email" className="form-control" name="email" />
-          </div>
-          <div className="form-group">
-            <label>Message:</label>
-            <textarea type="text" className="form-control" name="message" />
-          </div>
+      <form
+        className="d-flex flex-column justify-content-center"
+        onSubmit={this.submitForm}
+        action="https://formspree.io/f/mleoznyk"
+        method="POST"
+      >
+        <div className="form-group">
+          <label>Name:</label>
+          <input type="text" className="form-control" name="name" />
+        </div>
+        <div className="form-group">
+          <label>Email:</label>
+          <input type="email" className="form-control" name="email" />
+        </div>
+        <div className="form-group">
+          <label>Message:</label>
+          <textarea type="text" className="form-control" name="message" />
+        </div>
 
-          {status === "SUCCESS" ? (
-            <p>Thanks!</p>
-          ) : (
-            <StyledButton type="button" textAlign="center" center>
-              Submit
-            </StyledButton>
-          )}
-          {status === "ERROR" && <p>Ooops! There was an error.</p>}
-        </form>
-      </ContentWrapper>
+        {status === "SUCCESS" ? (
+          <p>Thanks!</p>
+        ) : (
+          <StyledButton type="button" textAlign="center" center>
+            Submit
+          </StyledButton>
+        )}
+        {status === "ERROR" && <p>Ooops! There was an error.</p>}
+      </form>
     )
   }
 
