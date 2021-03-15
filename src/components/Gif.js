b8acbf7efbacc691ced15ef587721ef4ff524fbb
bcherrera14 <bcherrera14@gmail.com>
2021-03-14 18:19:24 -0700

    add sifter app

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -2,12 +2,14 @@ import React from "react"
 import expense from "../../content/index/projects/expensetracker-app/expensetracker.gif"
 import snake from "../../content/index/projects/snake-app/snakegame_v1.gif"
 import starwars from "../../content/index/projects/starwars-app/starwars_v5.gif"
+import sifter from "../../content/index/projects/sifter-app/sifter_v1.gif"
 
 function ProjectImage(props) {
   const gifLibrary = {
     ExpenseTrackerApp: expense,
     SnakeApp: snake,
     StarWarsDirectory: starwars,
+    SifterApp: sifter,
   }
   let projectTitle = props
   projectTitle = projectTitle.project.split(" ").join("")
